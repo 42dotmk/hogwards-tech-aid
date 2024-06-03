07875c552b947befa7282882ce1663e9ced18627
Zhivko Kocev <zikekocev57@gmail.com>
2024-06-03 19:13:06 +0200

    Import GORM

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	_ "gorm.io/gorm"
+
+	_ "gorm.io/driver/postgres"
 )
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		return
+	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
