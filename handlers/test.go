af6957fea1e38956abc7672bd27f65769dcb81a5
Ivan Trajkov <ivche@trajkov.mk>
2024-06-03 20:30:42 +0200

    Add hello world page.

--- handlers/test.go
+++ handlers/test.go
@@ -2,8 +2,21 @@ package handlers
 
 import (
 	"net/http"
+	"github.com/angelofallars/htmx-go"
 )
 
+const html = `
+<html>
+<body>
+	<h1>Hello World</h1>
+</body>
+</html>
+`
+
 func TestHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(500)
+	if htmx.IsHTMX(r) {
+		w.Write([]byte("Hello World HTMX"))
+	} else {
+		w.Write([]byte(html))
+	}
 }
